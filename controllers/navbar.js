053048b7fdb02aea93ec56b8da3af8eadacd0bf5
SAWARE <abdoulaye.saware.external@atos.net>
2016-05-03 11:39:23 +0000

    get /login

--- controllers/navbar.js
+++ controllers/navbar.js
@@ -1,6 +1,6 @@
 angular.module('MyApp')
   .controller('NavbarCtrl', function($scope, $auth, $http, $cookieStore ) {
-    $http.get('/home').success(function(data){
+    $http.get('/login').success(function(data){
         console.log(data.isAuthenticated);
         $cookieStore.put('authentication', data.isAuthenticated);
         $scope.isAuthenticated = function(){ return  $cookieStore.get('authentication');};
