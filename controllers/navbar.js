c1cf2962b66e1ebbbe1aeeca431eb14db41f5cbc
SAWARE <abdoulaye.saware.external@atos.net>
2016-05-25 10:03:18 +0000

    Module angularJS name changing

--- controllers/navbar.js
+++ controllers/navbar.js
@@ -1,7 +1,10 @@
 angular.module('WebrtcApp')
-  .controller('NavbarCtrl', function($scope, $auth, $http, $cookieStore ) {
+  .controller('NavbarCtrl', function($scope, $auth, $http, $cookieStore, $location ) {
     $http.get('/login').success(function(data){
         $cookieStore.put('authentication', data.isAuthenticated);
         $scope.isAuthenticated = function(){ return  $cookieStore.get('authentication');};
+        if( $scope.isAuthenticated()){
+            $location.url('/home');
+        }
        })
   });
